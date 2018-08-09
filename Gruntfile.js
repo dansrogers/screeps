e9f1d74c44bd8262ab5673e1278fbe909f6007eb
Daniel Rogers <dsrogers@google.com>
2018-08-08 22:04:36 -0700

    Initial copy of tutorial files

---
+++ Gruntfile.js
@@ -0,0 +1,90 @@
+module.exports = function(grunt) {
+
+    var config = require('./.screeps.json')
+    var branch = grunt.option('branch') || config.branch;
+    var email = grunt.option('email') || config.email;
+    var password = grunt.option('password') || config.password;
+    var ptr = grunt.option('ptr') ? true : config.ptr
+
+    grunt.loadNpmTasks('grunt-screeps')
+    grunt.loadNpmTasks('grunt-contrib-clean')
+    grunt.loadNpmTasks('grunt-contrib-copy')
+    grunt.loadNpmTasks('grunt-file-append')
+    grunt.loadNpmTasks('grunt-jsbeautifier')
+
+    var currentdate = new Date();
+
+    // Output the current date and branch.
+    grunt.log.subhead('Task Start: ' + currentdate.toLocaleString())
+    grunt.log.writeln('Branch: ' + branch)
+
+    grunt.initConfig({
+        screeps: {
+            options: {
+                email: email,
+                password: password,
+                branch: branch,
+                ptr: ptr
+            },
+            dist: {
+                src: ['dist/*.js']
+            }
+        },
+
+        // Remove all files from the dist folder.
+        clean: {
+            'dist': ['dist']
+        },
+
+        // Copy all source files into the dist folder, flattening the folder 
+        // structure by converting path delimiters to underscores
+        copy: {
+            // Pushes the game code to the dist folder so it can be modified 
+            // before being send to the screeps server.
+            screeps: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: '**',
+                    dest: 'dist/',
+                    filter: 'isFile',
+                    rename: function(dest, src) {
+                        // Change the path name utilize underscores for folders
+                        return dest + src.replace(/\//g, '_');
+                    }
+                }],
+            }
+        },
+
+        // Add version variable using current timestamp.
+        file_append: {
+            versioning: {
+                files: [{
+                    append: "\nmodule.exports.SCRIPT_VERSION = " + currentdate.getTime() + ";\n",
+                    input: 'dist/version.js',
+                }]
+            }
+        },
+
+        // Apply code styling
+        jsbeautifier: {
+            modify: {
+                src: ["*.js", "src/**/*.js"],
+                options: {
+                    config: '.jsbeautifyrc'
+                }
+            },
+            verify: {
+                src: ["*.js", "src/**/*.js"],
+                options: {
+                    mode: 'VERIFY_ONLY',
+                    config: '.jsbeautifyrc'
+                }
+            }
+        }
+    })
+
+    grunt.registerTask('default', ['jsbeautifier:modify', 'clean', 'copy:screeps', 'file_append:versioning', 'screeps']);
+    grunt.registerTask('test', ['jsbeautifier:verify']);
+    grunt.registerTask('pretty', ['jsbeautifier:modify']);
+}
---
+++ src/main.js
@@ -0,0 +1,39 @@
+var version = require('version')
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+
+module.exports.loop = function() {
+    if (!Memory.SCRIPT_VERSION || Memory.SCRIPT_VERSION != version.SCRIPT_VERSION) {
+        Memory.SCRIPT_VERSION = version.SCRIPT_VERSION
+        console.log('New code uploaded')
+    }
+
+    var tower = Game.getObjectById('68d424855c9883692a1fe365');
+    if (tower) {
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if (closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
+
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if (closestHostile) {
+            tower.attack(closestHostile);
+        }
+    }
+
+    for (var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if (creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if (creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if (creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
+}
